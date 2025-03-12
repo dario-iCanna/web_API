d0c3a025c18a0a6418c70ed690f85dc7f1472dad
lai2-24 <lai2-24@itismeucci.com>
2025-03-12 10:24:01 +0100

    server

--- php/index.php
+++ php/index.php
@@ -1,4 +1,6 @@
 <?php
+use Psr\Http\Message\ResponseInterface as Response;
+use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
@@ -7,5 +9,9 @@ require __DIR__ . '/controllers/AlunniController.php';
 $app = AppFactory::create();
 
 $app->get('/alunni', "AlunniController:index");
+$app->get('/alunni/{id}', "AlunniController:view");
+$app->post('/alunni', "AlunniController:create");
+$app->put('/alunni/{id}', "AlunniController:update");
+$app->delete('/alunni/{id}', "AlunniController:destroy");
 
 $app->run();
